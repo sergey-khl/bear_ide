c87f5ba17a29ad64961a73f1f0abbbcb1ae33c49
Sergey <sergey.khl.dev@gmail.com>
2025-07-28 21:41:47 -0600

    better

--- install.sh
+++ install.sh
@@ -2,17 +2,16 @@ curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+rm nvim-linux-x86_64.tar.gz
 
 sudo apt-get update && sudo apt-get install -y ripgrep
-
-ln -s nvim ~/.config/nvim
 
 # make sure no one else has nvim before running
 rm -rf ~/.config/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.local/share/nvim
 
-git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+ln -s "$(pwd)"/nvim ~/.config/nvim
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
 unzip Hack.zip
@@ -22,4 +21,7 @@ rm LICENSE.md README.md
 fc-cache -fv
 fc-list
 
+rm Hack.zip
+
 gsettings set org.gnome.desktop.interface font-name 'Hack Nerd Font Regular 10'
+
