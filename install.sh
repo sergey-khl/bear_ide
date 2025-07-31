9a1337b69006189599ec61c35f654efe7d615692
Your Name <you@example.com>
2025-07-30 19:22:45 -0600

    make local and add tmux

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+sudo rm -rf ~/.local/nvim
+sudo tar -C ~/.local -xzf nvim-linux-x86_64.tar.gz
+mv ~/.local/nvim-linux-x86_64 ~/.local/nvim
+echo -e "\nexport PATH=\"\$HOME/.local/nvim/bin:\$PATH\"" >> ~/.bashrc
 rm nvim-linux-x86_64.tar.gz
 
+cd ~/.local/bin || exit
 sudo apt-get update && sudo apt-get install -y ripgrep
 
 # make sure no one else has nvim before running
-rm -rf ~/.config/nvim
-rm -rf ~/.local/state/nvim
-rm -rf ~/.local/share/nvim
+sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.local/state/nvim
+sudo rm -rf ~/.local/share/nvim
 
 ln -s "$(pwd)"/nvim ~/.config/nvim
 
@@ -19,9 +21,9 @@ mkdir -p $HOME/.local/share/fonts
 mv "$(pwd)"/*.ttf $HOME/.local/share/fonts/
 rm LICENSE.md README.md
 fc-cache -fv
-fc-list
 
 rm Hack.zip
 
 gsettings set org.gnome.desktop.interface font-name 'Hack Nerd Font Regular 10'
 
+sudo apt install tmux
